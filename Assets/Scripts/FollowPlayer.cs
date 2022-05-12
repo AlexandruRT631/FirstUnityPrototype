bdf6768501919327ce70b48dd8570388028f4875
AlexandruRT631 <allextodor@gmail.com>
2022-05-12 19:22:09 +0300

    movement update + new camera mode

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -7,7 +7,7 @@ public class FollowPlayer : MonoBehaviour
     public GameObject player;
     private Vector3 _positionOffset;
     private Quaternion _rotation;
-    private bool _fpc = false;
+    private int _fpc = 0;
 
     // Start is called before the first frame update
     public void Start()
@@ -21,19 +21,24 @@ public class FollowPlayer : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.V))
         {
-            _fpc = !_fpc;
+            _fpc = (_fpc + 1) % 3;
         }
 
         transform.position = player.transform.position;
-        if (_fpc)
+        if (_fpc == 1)
         {
             transform.Translate(Vector3.up * 2 + Vector3.forward * 3);
             transform.rotation = player.transform.rotation;
         }
-        else
+        else if (_fpc == 0)
         {
             transform.position += _positionOffset;
             transform.rotation = _rotation;
+        }
+        else
+        {
+            transform.position += Vector3.up * 100;
+            transform.rotation = Quaternion.LookRotation(Vector3.down, Vector3.up);
         }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,23 +8,58 @@ public class PlayerController : MonoBehaviour
     public float turnSpeed = 75.0f;
     public float horizontalInput;
     public float forwardInput;
-    
+    // public float distToGround = 1.0f;
+    // public bool grounded = false;
+    public Rigidbody rb;
+    // public Vector3 movement;
+    // private Vector3 _oldPos;
+    // public float currentVerticalSpeed = 0.0f;
+    public float maxVerticalSpeed = 0.1f;
+    public float fakeInput = 1.0f;
+
     // Start is called before the first frame update
     public void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        // _oldPos = transform.position;
     }
 
-    // Update is called once per frame
-    public void FixedUpdate()
+    public void Update()
     {
         // Getting player input
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-        
+        // transform.rotation.y returns values from -1 to 1 instead of -180 to 180
+        // var angle = transform.eulerAngles.y * Mathf.Deg2Rad;
+        // movement = new Vector3(Mathf.Sin(angle), 0, Mathf.Cos(angle)) * forwardInput;
+    }
 
+// Update is called once per frame
+    public void FixedUpdate()
+    {
+        // currentVerticalSpeed = Mathf.Abs(_oldPos.y - transform.position.y) * Time.deltaTime;
+        // _oldPos = transform.position;
+        
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // rb.AddForce(movement * Time.deltaTime * speed);
+        if (Mathf.Abs(rb.velocity.y) < maxVerticalSpeed)
+        {
+            fakeInput = forwardInput;
+            transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        }
+        else
+        {
+            if (fakeInput < 0.01f && fakeInput > -0.01f)
+            {
+                fakeInput = 0f;
+            }
+            else
+            {
+                fakeInput -= fakeInput / Mathf.Abs(fakeInput) * Time.deltaTime / speed;
+            }
+            transform.Translate(Vector3.forward * Time.deltaTime * speed * fakeInput);
+        }
+        // _oldPos = transform.position;
         // Turn the vehicle based on horizontal and forward input
         transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput * forwardInput);
     }
